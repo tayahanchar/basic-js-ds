b4fa121f41184ed7d4d17b218dbf54658f3881cc
Taya Hanchar <tayahanchar@gmail.com>
2022-10-13 13:58:21 +0300

    feat: add queue-task

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Queue {
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if(this.length === 0) {
+      this.head = new ListNode(value);
+    } else {
+      let current = this.head;
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = new ListNode(value);
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.head;
+    this.head = this.head.next;
+    this.length--;
+    return node.value;
+  }
+}
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
   }
 }
 
